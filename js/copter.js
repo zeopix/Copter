105c5810e2961008e5349f1e475f3c24118ca143
Iván Guillén <zeopix@gmail.com>
2011-09-13 12:54:41 +0200

    up

--- js/copter.js
+++ js/copter.js
@@ -1,7 +1,31 @@
 $(function(){
-
+	var easing = "easeOutBounce";
 //	startGame();
 	animateFall("#copter");
+
+	$("#container").click(function(){
+	});
+	
+	$("#control").focus();
+	$("#control").focusout(function(){
+		$(this).trigger('focus');
+	});
+	
+	$("#control").keydown(function(event) {
+  		if(event.keyCode == 38){
+  			$("#copter").stop().animate({
+				marginTop: "-200px"
+			}, {
+				duration: getTime(getFallDistance($("#copter"))),
+				easing: easing
+			});
+  		}
+   	});
+	
+	$("#control").keyup(function(event) {
+		animateFall("#copter");
+   	});
+
 });
 
 
@@ -16,15 +40,18 @@ function startGame(){
 		return parseInt($(element).parent().outerHeight()) - parseInt(getYPosition(element)) - parseInt($(element).outerHeight());
 	}
 	function getTime(distance){
-		var time = Math.ceil(Math.sqrt((50*distance)/(32.2/3600)));
+		var time = Math.ceil(Math.sqrt((80*distance)/(32.2/3600)));
 		return time;
 	}
 	function animateFall(element){
-	
+		var easing = "easeOutBounce";
+
 		$(element).stop().animate({
 			marginTop: getFallDistance($(element)) + "px"
 		}, {
 			duration: getTime(getFallDistance($(element))),
-			easing: "easeOutBounce"
+			easing: easing
 		});
+
+
 	}
